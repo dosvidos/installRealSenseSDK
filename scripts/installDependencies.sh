f45f39b4b870c5ce9e27b86c5e3023a0b21aeee5
Mikhail <dosvidos@users.noreply.github.com>
2019-12-03 01:57:24 +0300

    Do not install python3 and QtCreator

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -17,10 +17,3 @@ apt-get install build-essential cmake cmake-curses-gui -y
 
 # Graphics libraries - for SDK's OpenGL-enabled examples
 apt-get install libgtk-3-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev -y
-
-# QtCreator for development; not required for librealsense core library
-apt-get install qtcreator -y
-
-# Add Python 3 support
-apt-get install -y python3 python3-dev
-
